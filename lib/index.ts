07cd7b7cd81fd4c1a0d910c373fc209fe1e806d6
Nana Duah <gabano2005@gmail.com>
2023-04-17 21:12:50 -0400

    export deps

---
+++ lib/index.ts
@@ -0,0 +1,6 @@
+import * as apigatewayv2 from "@aws-cdk/aws-apigatewayv2-alpha";
+import * as authorizers from "@aws-cdk/aws-apigatewayv2-authorizers-alpha";
+import * as integrations from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+import * as lambdapython from "@aws-cdk/aws-lambda-python-alpha";
+
+export { apigatewayv2, authorizers, integrations, lambdapython };
